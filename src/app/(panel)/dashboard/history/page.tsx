e58e5109904d8630db1fc06447d64096a98a9224
alangt22 <alansilva2896@gmail.com>
2026-02-10 15:09:58 -0300

    fix: ajuste metadata do history

--- src/app/(panel)/dashboard/history/page.tsx
+++ src/app/(panel)/dashboard/history/page.tsx
@@ -2,6 +2,17 @@ import getSession from "@/lib/getSession";
 import { ListHistory } from "./_components/listHistory";
 import { redirect } from "next/navigation";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Historico | AgendaPRO",
+  description: "Historico - AgendaPRO",
+  robots:{
+    index: false,
+    follow: false,
+    nocache: true
+  },
+};
 
 export default async function History() {
   const session = await getSession();
